db78316f86cbdbcb47cae1633c1d8ffe4cfc1d94
stap780 <panaet80@mail.ru>
2024-05-08 21:29:39 +0300

    change to mini_magick

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,2 +1,2 @@
 Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
-Rails.application.config.active_storage.variant_processor = :vips
+Rails.application.config.active_storage.variant_processor = :mini_magick
