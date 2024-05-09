6bb22d1f8b9aa8a80cbcff19bc4c68f9bb4206c3
stap780 <panaet80@mail.ru>
2024-05-09 12:21:50 +0300

    add wss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://app.xn--80aamqmnl4e8c.xn--p1ai/cable"
+  config.action_cable.allowed_request_origins = ["https://app.xn--80aamqmnl4e8c.xn--p1ai"]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
