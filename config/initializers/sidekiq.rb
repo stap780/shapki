4cc026c41058de6e7a6af4104a8f837d888016bc
stap780 <panaet80@mail.ru>
2024-05-16 11:22:19 +0300

    add sidekiq scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,23 @@
+require "sidekiq"
+require "sidekiq-scheduler"
+
 Sidekiq.configure_server do |config|
     config.redis = {
       url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
     }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
+  }
+end
+
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    #   Sidekiq.schedule = YAML.load_file(Rails.root.join("config", "sidekiq_scheduler", "#{Rails.env}.yml")) || {}
+    Sidekiq.schedule = YAML.load_file(Rails.root.join("config", "sidekiq_scheduler.yml")) || {}
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = {
-      url: ENV.fetch("REDIS_URL", "redis://localhost:6379/1")
-    }
-  end
+end
